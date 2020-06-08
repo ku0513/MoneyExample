7f50f401f92dbfb90ae5dc6969b1141493d157c8
ku0513 <kenichi.umeda@gmail.com>
2020-06-08 14:19:00 +0900

    Chapter 8 実装を隠す

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,7 +1,17 @@
 package money;
 
-public class Money {
+abstract class Money {
     protected int amount;
+
+    abstract Money times(int multiplier);
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Franc franc(int amount) {
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
